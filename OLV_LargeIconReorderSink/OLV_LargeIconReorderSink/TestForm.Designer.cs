502ecb33a50df84ab44ef5c6247bdf6b59a99a7e
janek49 <68037466+janek49@users.noreply.github.com>
2021-05-06 22:27:01 +0200

    Add files via upload

---
+++ OLV_LargeIconReorderSink/OLV_LargeIconReorderSink/TestForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace OLV_LargeIconReorderSink
+{
+    partial class TestForm
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.objectListView1 = new BrightIdeasSoftware.ObjectListView();
+            this.btnAddItems = new System.Windows.Forms.Button();
+            this.btnCreateGroups = new System.Windows.Forms.Button();
+            this.olvcImage = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvcGroup = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            ((System.ComponentModel.ISupportInitialize)(this.objectListView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // objectListView1
+            // 
+            this.objectListView1.AllColumns.Add(this.olvcImage);
+            this.objectListView1.AllColumns.Add(this.olvcGroup);
+            this.objectListView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.objectListView1.CellEditUseWholeCell = false;
+            this.objectListView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.olvcImage,
+            this.olvcGroup});
+            this.objectListView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.objectListView1.FullRowSelect = true;
+            this.objectListView1.HasCollapsibleGroups = false;
+            this.objectListView1.HideSelection = false;
+            this.objectListView1.IsSearchOnSortColumn = false;
+            this.objectListView1.Location = new System.Drawing.Point(12, 47);
+            this.objectListView1.Name = "objectListView1";
+            this.objectListView1.ShowSortIndicators = false;
+            this.objectListView1.Size = new System.Drawing.Size(776, 391);
+            this.objectListView1.SortGroupItemsByPrimaryColumn = false;
+            this.objectListView1.TabIndex = 0;
+            this.objectListView1.UseCompatibleStateImageBehavior = false;
+            this.objectListView1.View = System.Windows.Forms.View.LargeIcon;
+            // 
+            // btnAddItems
+            // 
+            this.btnAddItems.Location = new System.Drawing.Point(12, 12);
+            this.btnAddItems.Name = "btnAddItems";
+            this.btnAddItems.Size = new System.Drawing.Size(75, 23);
+            this.btnAddItems.TabIndex = 1;
+            this.btnAddItems.Text = "Add Items";
+            this.btnAddItems.UseVisualStyleBackColor = true;
+            this.btnAddItems.Click += new System.EventHandler(this.btnAddItems_Click);
+            // 
+            // btnCreateGroups
+            // 
+            this.btnCreateGroups.Location = new System.Drawing.Point(93, 12);
+            this.btnCreateGroups.Name = "btnCreateGroups";
+            this.btnCreateGroups.Size = new System.Drawing.Size(105, 23);
+            this.btnCreateGroups.TabIndex = 2;
+            this.btnCreateGroups.Text = "Create Groups";
+            this.btnCreateGroups.UseVisualStyleBackColor = true;
+            this.btnCreateGroups.Click += new System.EventHandler(this.btnCreateGroups_Click);
+            // 
+            // olvcImage
+            // 
+            this.olvcImage.Text = "Image";
+            // 
+            // olvcGroup
+            // 
+            this.olvcGroup.Text = "Group";
+            // 
+            // TestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCreateGroups);
+            this.Controls.Add(this.btnAddItems);
+            this.Controls.Add(this.objectListView1);
+            this.Name = "TestForm";
+            this.Text = "Test";
+            ((System.ComponentModel.ISupportInitialize)(this.objectListView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private BrightIdeasSoftware.ObjectListView objectListView1;
+        private System.Windows.Forms.Button btnAddItems;
+        private System.Windows.Forms.Button btnCreateGroups;
+        private BrightIdeasSoftware.OLVColumn olvcImage;
+        private BrightIdeasSoftware.OLVColumn olvcGroup;
+    }
+}
+
---
+++ OLV_LargeIconReorderSink/OLV_LargeIconReorderSink/TestForm.cs
@@ -0,0 +1,164 @@
+﻿using BrightIdeasSoftware;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OLV_LargeIconReorderSink
+{
+    public partial class TestForm : Form
+    {
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+
+        public class ImageFile
+        {
+            public string Path;
+            public Image Image;
+            public int GroupNo = 0;
+
+            public ImageFile(string path)
+            {
+                Path = path;
+            }
+
+            public void LoadImage()
+            {
+                Image = Image.FromFile(Path);
+            }
+        }
+
+
+        private void btnAddItems_Click(object sender, EventArgs e)
+        {
+            var imagePaths = new string[] { "testfiles\\kot.jpg", "testfiles\\pies.jpg", "testfiles\\lew.jpg" };
+
+            var list = new List<ImageFile>();
+            ImageList imgList = new ImageList();
+            imgList.ImageSize = new Size(128, 128);
+            imgList.ColorDepth = ColorDepth.Depth24Bit;
+
+            foreach (var path in imagePaths)
+            {
+                var imgFile = new ImageFile(path);
+                imgFile.LoadImage();
+                list.Add(imgFile);
+                imgList.Images.Add(imgFile.Path, imgFile.Image);
+            }
+
+            objectListView1.LargeImageList = imgList;
+            objectListView1.OwnerDraw = true;
+
+            olvcImage.ImageGetter = delegate (object row)
+            {
+                return ((ImageFile)row).Path;
+            };
+            olvcImage.AspectGetter = delegate (object row)
+            {
+                return ((ImageFile)row).Path;
+            };
+         
+            olvcGroup.AspectGetter = delegate (object row)
+            {
+                return ((ImageFile)row).GroupNo;
+            };
+
+            olvcImage.Sortable = false;
+            olvcGroup.Sortable = true;
+
+            objectListView1.SetObjects(list);
+
+            objectListView1.DragSource = new SimpleDragSource();
+            
+            var sink = new LargeIconReorderSink();
+
+            sink.CanDropOnItem = true;
+            sink.CanDropOnBackground = true;
+            sink.CanDropOnSubItem = true;
+
+            sink.CanDropBetween = true;
+            
+            sink.CanDrop += Sink_CanDrop;
+            sink.ModelDropped += Sink_ModelDropped;
+            objectListView1.DropSink = sink;
+
+            objectListView1.AllowDrop = true;
+
+            objectListView1.ShowGroups = true;
+            objectListView1.AlwaysGroupByColumn = olvcGroup;
+            objectListView1.BeforeSorting += ObjectListView1_BeforeSorting;
+
+            objectListView1.ShowItemCountOnGroups = true;
+
+            objectListView1.RowHeight = 128;
+            objectListView1.SortGroupItemsByPrimaryColumn = false;
+            objectListView1.UseCompatibleStateImageBehavior = false;
+
+            RefreshListView();
+        }
+
+        private void ObjectListView1_BeforeSorting(object sender, BeforeSortingEventArgs e)
+        {
+            var s = new OLVColumn();
+            s.AspectGetter = (o) => objectListView1.IndexOf(o);
+
+            objectListView1.ListViewItemSorter = new ColumnComparer(
+                        s, SortOrder.Ascending, e.ColumnToSort, e.SortOrder);
+            e.Handled = true;
+        }
+
+        private void RefreshListView()
+        {
+            objectListView1.SetObjects(objectListView1.Objects);
+            objectListView1.BuildList(true);
+            objectListView1.BuildGroups();
+            objectListView1.Groups.Clear();
+            objectListView1.Sort();
+            objectListView1.BuildGroups();
+            objectListView1.Update();
+            objectListView1.Refresh();
+        }
+
+        private void Sink_ModelDropped(object sender, ModelDropEventArgs e)
+        {
+            if (e.DropTargetLocation != DropTargetLocation.AboveItem && e.DropTargetLocation != DropTargetLocation.BelowItem)
+                return;
+
+            var sources = e.SourceModels;
+            var target = e.TargetModel as ImageFile;
+
+            foreach (var obj in sources)
+            {
+                var model = obj as ImageFile;
+                if (model != null)
+                    model.GroupNo = target.GroupNo;
+            }
+            e.RefreshObjects();
+            RefreshListView();
+        }
+
+        private void Sink_CanDrop(object sender, OlvDropEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+            e.DropSink.FeedbackColor = Color.Green;
+
+        }
+
+        private void btnCreateGroups_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+            foreach (var obj in objectListView1.Objects)
+            {
+                (obj as ImageFile).GroupNo = counter++;
+            }
+            RefreshListView();
+        }
+    }
+}
